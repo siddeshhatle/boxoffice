21aa188972f39baa6b323dc0060cccbcde32c1e3
siddeshhatle <siddeshhatle47@gmail.com>
2022-11-17 15:57:52 +0530

    addded nav bar and created page commponent

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Navs from './components/Nav'
+import Home from './pages/Home'
+import Test from './pages/Test'
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is Homepage.  
-      </Route>
+    <div>
 
-      <Route exact path="/test">
-        This is of Test.  
-      </Route>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route>
-        404|Not Found
-      </Route>
-    </Switch>
+        <Route exact path="/test">
+          <Test /> 
+        </Route>
+
+        <Route>
+          404|Not Found
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
