2fb8ba3eda10f4dc8d9af5de69f440c18103f3fe
siddeshhatle <siddeshhatle47@gmail.com>
2022-11-30 11:04:43 +0530

    installed styled components and created styles on serach cards

--- src/components/actor/ActorGrid.jsx
+++ src/components/actor/ActorGrid.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ActorCard from './ActorCard'
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
 
 const ActorGrid = ({ data }) => {
     return (
-        <div> 
+        <FlexGrid> 
             {
                 data.map(({ person }) => (
                     <ActorCard 
@@ -19,7 +20,7 @@ const ActorGrid = ({ data }) => {
                     />
                 ))
             }
-        </div>
+        </FlexGrid>
     )
 }
 
