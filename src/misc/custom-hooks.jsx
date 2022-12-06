f18b51cc412807b3df28eae32a4d4f38480b863f
siddeshhatle <siddeshhatle47@gmail.com>
2022-12-06 18:30:47 +0530

    using custome hooks for starred shows

--- src/misc/custom-hooks.jsx
+++ src/misc/custom-hooks.jsx
@@ -16,7 +16,7 @@ function showsReducer(prevState, action){
 }
 
 function usePersistedReducer(reducer, initialState, key){
-    const { state,  dispatch } = useReducer(reducer, initialState, initial => {
+    const [ state,  dispatch ] = useReducer(reducer, initialState, initial => {
         const persisted = localStorage.getItem(key);
 
         return persisted ? JSON.parse(persisted) : initial;
@@ -25,6 +25,8 @@ function usePersistedReducer(reducer, initialState, key){
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(state))
     }, [state, key])
+
+    return [state, dispatch]
 }
 
 export function useShows (key = 'shows'){
