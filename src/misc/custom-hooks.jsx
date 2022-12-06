87ab35270eba22949a7c62915d56e14e1afa0281
siddeshhatle <siddeshhatle47@gmail.com>
2022-12-06 19:41:43 +0530

    created two new more hooks :useLastQuery and useShow

--- src/misc/custom-hooks.jsx
+++ src/misc/custom-hooks.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { useReducer, useEffect } from 'react';
+import { apiGet } from './config';
 
 function showsReducer(prevState, action){
     switch(action.type){
@@ -31,4 +33,71 @@ function usePersistedReducer(reducer, initialState, key){
 
 export function useShows (key = 'shows'){
     return usePersistedReducer(showsReducer, [], key)
+}
+
+export function useLastQuery(key = 'lastQuery'){
+    const [input, setInput] = useState(() => {
+        const persisted = sessionStorage.getItem(key);
+
+        return persisted ? JSON.parse(persisted) : "";
+    })
+
+    const setPersistedInput = newState => {
+        setInput(newState);
+        sessionStorage.setItem(key, JSON.stringify(newState));
+    }
+
+    return [input, setPersistedInput];
+}
+
+
+const reducer = (prevState, action) => {
+    switch (action.type) {
+      case 'FETCH_SUCCESS' : {
+        return {isloading : false, error: null, show : action.show}
+      }
+  
+      case 'FETCH_FAIL' : {
+        return {...prevState, isloading : false, error : action.error}
+      }
+  
+      default:
+        return prevState;
+    }
+  }
+
+
+
+export function useShow(showId){
+    
+    const [state, dispatch] = useReducer(
+        reducer,
+        {
+            show : null,
+            isLoading : true,
+            error : null,
+        }
+    );
+    useEffect(() => {
+        let isMounted = true;
+          apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`).then(
+            
+            result => {
+              setTimeout(() => {
+                if(isMounted){
+                  dispatch({type: 'FETCH_SUCCESS', show: result});
+                }
+              }, 0);
+          }).catch(err => {
+            if(isMounted){
+              dispatch({type: 'FETCH_FAIL', error: err.message});
+            }
+          });
+  
+          return () => {
+            isMounted = false;
+          }
+      }, [showId]);
+
+      return state;
 }
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,66 +1,23 @@
-import React, { useEffect, useReducer } from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom'
 import Cast from '../components/show/Cast';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
-import { apiGet } from '../misc/config';
+import { useShow } from '../misc/custom-hooks';
 import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
-
-const reducer = (prevState, action) => {
-  switch (action.type) {
-    case 'FETCH_SUCCESS' : {
-      return {isloading : false, error: null, show : action.show}
-    }
-
-    case 'FETCH_FAIL' : {
-      return {...prevState, isloading : false, error : action.error}
-    }
-
-    default:
-      return prevState;
-  }
-}
-
-const initialState = {
-  show : null,
-  isLoading : true,
-  error : null,
-}
 
 const Show = () => {
     const { id } = useParams();
 
-    const [{ show, isLoading, error }, dispatch] = useReducer(
-      reducer,
-      initialState
-    );
+    const { show, isLoading, error } = useShow(id);
 
     // const [show, setShow] = useState(null);
     // const [isLoading, setIsLoading] = useState(true);
     // const [error, setError] = useState(null);
 
-    useEffect(() => {
-      let isMounted = true;
-        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(
-          
-          result => {
-            setTimeout(() => {
-              if(isMounted){
-                dispatch({type: 'FETCH_SUCCESS', show: result});
-              }
-            }, 0);
-        }).catch(err => {
-          if(isMounted){
-            dispatch({type: 'FETCH_FAIL', error: err.message});
-          }
-        });
-
-        return () => {
-          isMounted = false;
-        }
-    }, [id]);
+  
     console.log('show', show);
     // console.log('isLoading', isLoading);
     // console.log('error', error);
